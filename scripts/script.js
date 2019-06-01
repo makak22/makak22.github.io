d4afcb46221791f48f32b5887738d92b2cc3a264
makak22 <43416202+makak22@users.noreply.github.com>
2019-06-01 20:54:30 +0000

    final01
    enfin!!!

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,13 @@ const aleatoire = Math.floor(Math.random() * citations.length);
 const blockquote = document.querySelector('.quotes');
 blockquote.textContent = citations[aleatoire];
 
+document.querySelector('button').addEventListener('click', function(){
+const aleatoire = Math.floor(Math.random() * citations.length);
+    const blockquote = document.querySelector('.quotes');
+    blockquote.textContent = citations[aleatoire];
+})
+
+
 var shakeEvent = new Shake({threshold: 15});
     shakeEvent.start();
     window.addEventListener('shake', function(){
@@ -18,8 +25,3 @@ var shakeEvent = new Shake({threshold: 15});
     //check if shake is supported or not.
     if(!("ondevicemotion" in window)){alert("Not Supported");}
 
-document.querySelector('button').addEventListener('click', function(){
-const aleatoire = Math.floor(Math.random() * citations.length);
-    const blockquote = document.querySelector('.quotes');
-    blockquote.textContent = citations[aleatoire];
-})
