27c698de448cec7ae06cd44217b83d4663974357
makak22 <43416202+makak22@users.noreply.github.com>
2019-08-19 08:39:27 +0000

    generateur decitation

--- scripts/quotes.js
+++ scripts/quotes.js
@@ -1,2 +1,16 @@
-const citations = ['La priere est une arme qui ne rate jamais sa cible', 'C\'est en forgeant que on devient forgeront', 'God has promised to supply all our needs. What we don’t have now, we don’t need now.', 'No matter what our circumstances, we can find a reason to be thankful.', 'If I succeed, I will give thanks. If I fail, I will seek His grace', 'It is only with gratitude that life becomes rich!', 'God is in control, and therefore in EVERYTHING I can give thanks - not because of the situation but because of the One who directs and rules over it.', 'A sensible thanksgiving for mercies received is a mighty prayer in the Spirit of God. It prevails with Him unspeakably.', 'Gratitude is an offering precious in the sight of God, and it is one that the poorest of us can make and be not poorer but richer for having made it.', 'Genuine thankfulness is an act of the heart’s affections, not an act of the lips’ muscles.', 'I have held many things in my hands, and I have lost them all; but whatever I have placed in God’s hands, that, I still possess.', 'It’s one thing to be grateful. It’s another to give thanks. Gratitude is what you feel. Thanksgiving is what you do', 'When we choose thankful prayer over wallowing in anxiety and worry, we are demonstrating an unwavering trust in God.', 'When it comes to life the critical thing is whether you take things for granted or take them with gratitude.', 'Gratitude\' comes from the same word as freedom (gratis = free). Gratitude is the freeing expression of a free heart toward one who freely gave'];
+const citations = ['La priere est une arme qui ne rate jamais sa cible',
+'C\'est en forgeant que on devient forgeront',
+'God has promised to supply all our needs. What we don’t have now, we don’t need now.',
+'No matter what our circumstances, we can find a reason to be thankful.',
+'If I succeed, I will give thanks. If I fail, I will seek His grace',
+'It is only with gratitude that life becomes rich!',
+'God is in control, and therefore in EVERYTHING I can give thanks - not because of the situation but because of the One who directs and rules over it.',
+'A sensible thanksgiving for mercies received is a mighty prayer in the Spirit of God. It prevails with Him unspeakably.',
+'Gratitude is an offering precious in the sight of God, and it is one that the poorest of us can make and be not poorer but richer for having made it.', 
+'Genuine thankfulness is an act of the heart’s affections, not an act of the lips’ muscles.',
+'I have held many things in my hands, and I have lost them all; but whatever I have placed in God’s hands, that, I still possess.',
+'It’s one thing to be grateful. It’s another to give thanks. Gratitude is what you feel. Thanksgiving is what you do', 'When we choose thankful prayer over wallowing in anxiety and worry, we are demonstrating an unwavering trust in God.', 
+'When it comes to life the critical thing is whether you take things for granted or take them with gratitude.',
+'Gratitude\' comes from the same word as freedom (gratis = free). Gratitude is the freeing expression of a free heart toward one who freely gave'
+];
 
